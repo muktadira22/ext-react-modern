f55e9ebd1199dd7fb157c2538096e9b76d4d3dbe
muktadira22 <aryamuktadir22@gmail.com>
2024-06-30 14:28:29 +0700

    Updates

---
+++ precache-manifest.e422d327355194bd1b9e0e6ac475a60f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a0591f6ac12c1882a1701f0f38569d",
+    "url": "/ext-react-modern/index.html"
+  },
+  {
+    "revision": "1287802536d13435f2e2",
+    "url": "/ext-react-modern/static/css/main.a3353f2a.chunk.css"
+  },
+  {
+    "revision": "c1479b19cd51685b0034",
+    "url": "/ext-react-modern/static/js/2.b6ba7eb7.chunk.js"
+  },
+  {
+    "revision": "1287802536d13435f2e2",
+    "url": "/ext-react-modern/static/js/main.fcca99e3.chunk.js"
+  },
+  {
+    "revision": "03a518bed30c1650138b",
+    "url": "/ext-react-modern/static/js/runtime~main.992265fd.js"
+  },
+  {
+    "revision": "3d6733827f3e5026323132e571bc9454",
+    "url": "/ext-react-modern/static/media/SenchaLogoLg.3d673382.svg"
+  }
+]);
